2ad9617dcf3e57d6ce088f11eebe3db81df3b047
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-03-08 15:44:27 -0300

    Color Picker Chrome Extension

---
+++ script.js
@@ -0,0 +1,17 @@
+const colorPickerBtn = document.querySelector("#color-picker");
+const pickedColors = [];
+
+const activateEyeDropper = async () => {
+    try {
+        const eyeDropper = new EyeDropper();
+        const {sRGBHex} = await eyeDropper.open();
+        navigator.clipboard.writeText(sRGBHex);
+
+        pickedColors.push(sRGBHex);
+        localStorage.setItem("picked-colors", JSON.stringify(pickedColors));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+colorPickerBtn.addEventListener("click", activateEyeDropper);
